165136914d62443d452e11a200f58b1deb264a6c
sidhantborkar7-prog <sidhantborkar7@gmail.com>
2026-07-28 13:34:23 +0530

    ALU logic added with opcode

---
+++ rtl/alu.v
@@ -0,0 +1,132 @@
+`timescale 1ns/1ps 
+
+module alu(
+    input [7:0]A,
+    input [7:0]B,
+    input [2:0]opcode,
+
+    output reg [7:0]result,
+    output reg carry,
+    output reg overflow
+);
+
+//arithmatic unit  
+wire [7:0]arithmetic_result;
+wire arithmetic_cout;
+wire arithmetic_overflow;
+wire sub;
+
+assign sub = (opcode == 3'b001);
+
+//logical unit 
+wire [7:0]logical_result;
+reg [1:0]logical_op;
+
+always @(*) begin
+    case(opcode)
+        3'b010 : logical_op = 2'b00; //AND 
+        3'b011 : logical_op = 2'b01; //OR
+        3'b100 : logical_op = 2'b10; //XOR
+        3'b101 : logical_op = 2'b11; //NAND
+        default : logical_op = 2'b00; 
+    endcase
+end 
+
+//shift unit 
+wire [7:0]shift_result;
+wire shift_carry;
+wire shift_op;
+
+assign shift_op = opcode[0];
+
+arithmetic_unit u1(
+    .A(A),
+    .B(B),
+    .sub(sub),
+    .result(arithmetic_result),
+    .cout(arithmetic_cout),
+    .overflow(arithmetic_overflow)
+);
+
+logical_unit u2(
+    .A(A),
+    .B(B),
+    .opcode(logical_op),
+    .result(logical_result)
+);
+
+shift_unit u3(
+    .A(A),
+    .shift_op(shift_op),
+    .result(shift_result),
+    .shift_carry(shift_carry)
+);
+
+always @(*) begin
+    case(opcode)
+        //ADD
+        3'b000 : begin
+            result = arithmetic_result;
+            carry = arithmetic_cout;
+            overflow = arithmetic_overflow;
+        end 
+        
+        //SUB
+        3'b001 : begin
+            result = arithmetic_result;
+            carry = arithmetic_cout;
+            overflow = arithmetic_overflow;
+        end
+
+        //AND
+        3'b010 : begin
+            result = logical_result;
+            carry = 1'b0;
+            overflow = 1'b0;
+        end
+
+        //OR
+        3'b011 : begin
+            result = logical_result;
+            carry = 1'b0;
+            overflow = 1'b0;
+        end
+
+        //XOR
+        3'b100 : begin
+            result = logical_result;
+            carry = 1'b0;
+            overflow = 1'b0;
+        end
+
+        //NAND
+        3'b101 : begin
+            result = logical_result;
+            carry = 1'b0;
+            overflow = 1'b0;
+        end
+
+        //Left shift 
+        3'b110 : begin
+            result = shift_result;
+            carry = shift_carry;
+            overflow = 1'b0;
+        end
+
+        //Right shift 
+        3'b111 : begin
+            result = shift_result;
+            carry = shift_carry;
+            overflow = 1'b0;
+        end
+
+        //default case 
+        default : begin
+            result = 8'b00000000;
+            carry = 1'b0;
+            overflow = 1'b0;
+        end
+    endcase
+
+end 
+endmodule
---
+++ tb/alu_TB.v
@@ -0,0 +1,80 @@
+`timescale 1ns/1ps
+
+module alu_tb;
+
+reg [7:0]A;
+reg [7:0]B;
+reg [2:0]opcode;
+
+wire [7:0]result;
+wire carry;
+wire overflow;
+
+alu dut(
+    .A(A),
+    .B(B),
+    .opcode(opcode),
+    .result(result),
+    .carry(carry),
+    .overflow(overflow)
+);
+
+initial begin
+    $dumpfile("wave/alu.vcd");
+    $dumpvars(0 , alu_tb);
+
+    //case 1 : ADD 5 + 15 = 20 
+    A = 8'b00000101;
+    B = 8'b00001111;
+    opcode = 3'b000;
+    #10;
+
+    // case 2 : ADD 127+1 =128 overflow = 1
+    A = 8'b01111111;
+    B = 8'b00000001;
+    opcode = 3'b000;
+    #10;
+
+    //case 3 : SUB 15 - 10 = 5 
+    A = 8'b00001111;
+    B = 8'b00001010;
+    opcode = 3'b001;
+    #10;
+
+    //case 4 : AND 
+    A = 8'b00001111;
+    B = 8'b00001010;
+    opcode = 3'b010;
+    #10;
+
+    //case 5 : OR 
+    A = 8'b00001111;
+    B = 8'b00001010;
+    opcode = 3'b011;
+    #10;
+
+    //case 6 : XOR 
+    A = 8'b00001111;
+    B = 8'b00001010;
+    opcode = 3'b100;
+    #10;
+
+    //case 7 : NAND
+    A = 8'b00001111;
+    B = 8'b00001010;
+    opcode = 3'b101;
+    #10;
+
+    //case 8 : Left shift  
+    A = 8'b00001111;
+    opcode = 3'b110;
+    #10;
+
+    //case 9 : Right shift  
+    A = 8'b00001111;
+    opcode = 3'b111;
+    #10;
+
+    $finish;
+end 
+endmodule
